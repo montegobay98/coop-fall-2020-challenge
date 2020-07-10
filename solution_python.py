ac46ef8ad67ad9bc5d4bee83124dce2337732b19
Momo <36833672+montegobay98@users.noreply.github.com>
2020-07-10 14:06:10 -0400

    Quick fix

--- solution_python.py
+++ solution_python.py
@@ -10,20 +10,25 @@ class EventSourcer():
         # Using 0's and 1's might be more memory intensive compared to making a number negative,
         # but in the end it shouldn't really matter because it's 1 extra bit of information. It's also more versatile
         # against edge cases than making a number negative (i.e. inputting a negative number).
+
+        # Solution space complexity: O(N)
         self.event_history = []
         self.redo_stack = []
 
+    # Runtime complexity: O(1)
     def add(self, num: int):
         self.value += num
         self.event_history.append((num, False))
         # Could be cleaner but one liners rule supreme
         self.redo_stack.pop() if len(self.redo_stack) != 0 else None
 
+    # Runtime complexity: O(1)
     def subtract(self, num: int):
         self.value -= num
         self.event_history.append((num, True))
         self.redo_stack.pop() if len(self.redo_stack) != 0 else None
 
+    # Runtime complexity: O(1)
     def undo(self):
         if len(self.event_history) > 0:
             last_event = self.event_history.pop()
@@ -32,17 +37,22 @@ class EventSourcer():
             # Insert at the beginning of the list
             self.redo_stack.append(last_event)
 
+    # Runtime complexity: O(1)
     def redo(self):
         if len(self.redo_stack) > 0:
             last_event = self.redo_stack.pop()
             self.value = self.value + last_event[0] if not last_event[1] else self.value - last_event[0]
 
+    # Runtime complexity: O(N)
     def bulk_undo(self, steps: int):
         for i in range(steps):
             if len(self.event_history) > 0:
                 self.undo()
 
+    # Runtime complexity: O(N)
     def bulk_redo(self, steps: int):
         for i in range(steps):
             if len(self.redo_stack) > 0:
                 self.redo()
+            else:
+                break
